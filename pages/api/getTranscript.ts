f1b83678337eda9943457db4eba16c38f31cf0e0
Kevin Riste <kevinbobriste@gmail.com>
2024-08-22 16:33:22 +0000

    Handle main video language not being 'en'

--- pages/api/getTranscript.ts
+++ pages/api/getTranscript.ts
@@ -1,7 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import getVideoId from 'get-video-id';
-import { YoutubeTranscript } from 'youtube-transcript';
+import { YoutubeTranscript, YoutubeTranscriptNotAvailableLanguageError } from 'youtube-transcript';
 import { decode } from 'html-entities';
 
 const handler = async (
@@ -11,25 +10,49 @@ const handler = async (
   const body = JSON.parse(req.body);
   const ytUrlInput = body.yturl || '';
 
-  if (ytUrlInput === '') res.status(500).send('YouTube URL not provided.');
+  if (ytUrlInput === '') {
+    res.status(500).send('YouTube URL not provided.');
+    return;
+  }
 
-  else {
+  try {
+    const { id: ytVideoId } = getVideoId(ytUrlInput);
+
+    let transcriptFromNpmVideoId;
     try {
-      const { id: ytVideoId } = getVideoId(ytUrlInput);
-
-      const transcriptFromNpmVideoId = await YoutubeTranscript.fetchTranscript(ytVideoId || '', {
+      transcriptFromNpmVideoId = await YoutubeTranscript.fetchTranscript(ytVideoId || '', {
         lang: 'en'
       });
-
-      const joinedTranscript = transcriptFromNpmVideoId.map(transcriptPart => transcriptPart.text).join(' ');
-
-      const finalTranscript = decode(decode(joinedTranscript));
-
-      res.status(200).json(finalTranscript)
     } catch (error: any) {
-      console.error(error);
-      res.status(500).send(error.message);
+      if (error instanceof YoutubeTranscriptNotAvailableLanguageError) {
+        const availableLangsMatch = error.message.match(/Available languages: (.+)/);
+        if (availableLangsMatch) {
+          const availableLangs = availableLangsMatch[1].split(',').map(lang => lang.trim());
+          console.log('Retrying with available languages:', availableLangs);
+          if (availableLangs.length > 0) {
+            // Retry with the first available language
+            transcriptFromNpmVideoId = await YoutubeTranscript.fetchTranscript(ytVideoId || '', {
+              lang: availableLangs[0]
+            });
+          } else {
+            throw new Error('No transcripts available in any language for this video.');
+          }
+        } else {
+          throw new Error('No available languages could be parsed from the error message.');
+        }
+      } else {
+        throw error;
+      }
     }
+
+    const joinedTranscript = transcriptFromNpmVideoId.map(transcriptPart => transcriptPart.text).join(' ');
+
+    const finalTranscript = decode(decode(joinedTranscript));
+
+    res.status(200).json(finalTranscript);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).send(error.message);
   }
 }
 
