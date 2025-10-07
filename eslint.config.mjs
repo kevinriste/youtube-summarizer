c87f63747c489e22007e820e7016b4fdec19c55f
Kevin Riste <kevinbobriste@gmail.com>
2025-10-07 19:05:34 +0000

    Set up typescript-eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { FlatCompat } from '@eslint/eslintrc';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+export default defineConfig([
+  { ignores: ['.next/**'] },
+  {
+    languageOptions: { parserOptions: { projectService: true } },
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+]);
